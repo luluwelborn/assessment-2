b9ffaf9fae72594fac001174961b6d920e6ad2f0
Lauren Welborn <laurenwelborn@Laurens-MacBook-Pro.local>
2017-07-10 20:23:49 -0600

    updating js

--- script.js
+++ script.js
@@ -1,71 +1,58 @@
 
 //define variables - same as using document.getElementById('redSquare');
-// var redSquare = $('#redSquare');
-// var blueSquare = $('#blueSquare');
+var redSquare = $('#redSquare');
+var blueSquare = $('#blueSquare');
+// var finishLine = $('#finishLine');
 
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_event_keydown_keyup
+$(document).ready(function(){
+    
+    $("input").css("position", "absolute");
 
-// SOLUTION 3 - simple jQuery event moving 30px increments
-$("html").keydown(function(event){
-    if(event.which == "37") $("#redSquare").animate({"left" : "-=30px"}, { queue : false });
-
-    if(event.which == "39") $("#redSquare").animate({"left" : "+=30px"}, { queue : false });
-
-    if(event.which == "38") $("#redSquare").animate({"top" : "-=30px"}, { queue : false });
-
-    if(event.which == "40") $("#redSquare").animate({"top" : "+=30px"}, { queue : false });
+    $("input").keydown(function(event){
+        if(event.keyCode===38){
+            console.log("moved up");
+            // $("#redSquare").animate(direction)) continue;
+        }
+        else if(event.keyCode===40){
+            console.log("moved down");
+        }
+        $("input").css("right");
+        $("input").css("right", "20px");
+    });
+    
 });
 
 
-// ............................................................
 
-/*  SOLUTION 2
-    var keys = [];
-    $("html").keydown(function(event){
 
-    if (keys.length > 2) return;
-    keys.push (event.which);
-    
-    moveRedSquare();
+/*var keys = {};
+  $(document).keys(moveSquare(event){  
 });
+*/
 
-function moveRedSquare () {
-    var moveTo = keys[0];
-    if(moveTo == "37") {
-      $("#redSquare").stop(true).animate({"left" : "-=30px"});
-    }
 
-    if(moveTo == "39") {
-      $("#redSquare").stop(true).animate({"left" : "+=30px"});
-    }
+//if statements to move square 30px on each keydown press
+/*function moveSquare () {
+    for ()  {
+  if(moveTo == "37") {
+    $("#redSquare").stop(true).animate({"left" : "-=30px"});
+  }
 
-    if(moveTo == "38") {
-      $("#redSquare").stop(true).animate({"top" : "-=30px"});
-    }
+  if(moveTo == "39") {
+    $("#redSquare").stop(true).animate({"left" : "+=30px"});
+  }
 
-    if(moveTo == "40") {
-      $("#redSquare").stop(true).animate({"top" : "+=30px"});
-    }
-    
+  if(moveTo == "38") {
+    $("#redSquare").stop(true).animate({"top" : "-=30px"});
+  }
+
+  if(moveTo == "40") {
+    $("#redSquare").stop(true).animate({"top" : "+=30px"});
+  }
+    // slice() method returns portion of array into a new array object
     keys = keys.slice (1);
 }
+}
 
-
-//keydown event
-/*$(document).keydown(funtion(move)); {
-	$("redSquare").animate();
-	//keydown move value
-	right: '+ = 20px';
-};
-
-
-............................................................
-
-// SOLUTION 1 - .animate with button
-/*$(document).ready(function(){
-    $("button").click(function(){
-        $(".redSquare").animate({
-            left: '20px',,
-            //width: '+=20px'
-        });
-    });
-});
+moveSquare();
