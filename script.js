d32533b6dfb3ed0fb3533ad24ef9a50244858a11
Lauren Welborn <laurenwelborn@Laurens-MacBook-Pro.local>
2017-07-12 12:11:12 -0600

    backup code

--- script.js
+++ script.js
@@ -1,58 +1,63 @@
-
-//define variables - same as using document.getElementById('redSquare');
-var redSquare = $('#redSquare');
-var blueSquare = $('#blueSquare');
-// var finishLine = $('#finishLine');
+// define key codes & squares
+var redSquare = $("redSquare");
 
 // https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_event_keydown_keyup
-$(document).ready(function(){
-    
-    $("input").css("position", "absolute");
+$( document ).ready(function() {
+  $("redSquare").css("position", "absolute");
 
-    $("input").keydown(function(event){
+/*$("redSquare").keydown(function(event){
         if(event.keyCode===38){
             console.log("moved up");
+            $("#redSquare").css("right", "+20px");
             // $("#redSquare").animate(direction)) continue;
         }
         else if(event.keyCode===40){
             console.log("moved down");
-        }
-        $("input").css("right");
-        $("input").css("right", "20px");
-    });
+        } */
+  $("redSquare").keypress(function( event ) {
+    if (event.which == 38) {
+     console.log("keypressed up!");
+    }
+  });
+});
     
-});
 
+/*    
+};
+var keys = {};
+keys.UP = 38;
+keys.DOWN = 40;
 
+var redSquare = {
+  x: 50,
+  y: 50,
+  element: document.getElementById("redSquare")
+};
 
+// detecting the keys
+document.body.onkeyup =
+document.body.onkeydown = function(e){
+  var kc = e.keyCode || e.which;
+  keys[kc] = e.type == 'keydown';
+};
 
-/*var keys = {};
-  $(document).keys(moveSquare(event){  
-});
-*/
+// i found dx/dy to set coordinates, might not be best practices
+var moveredSquare = function(dx, dy){
+    redSquare.x += dx||0;
+    redSquare.y += dy||0;
+    redSquare.element.style.left = redSquare.x + 'px';
+    redSquare.element.style.top = redSquare.y + 'px';
+};
 
+//move with keys
+var detectredSquareMovement = function(){
+      if ( keys[keys.UP] ) {
+        moveredSquare(0, -5);
+      }
+      if ( keys[keys.DOWN] ) {
+        moveredSquare(0, 5);
+      }
+    };
 
-//if statements to move square 30px on each keydown press
-/*function moveSquare () {
-    for ()  {
-  if(moveTo == "37") {
-    $("#redSquare").stop(true).animate({"left" : "-=30px"});
-  }
-
-  if(moveTo == "39") {
-    $("#redSquare").stop(true).animate({"left" : "+=30px"});
-  }
-
-  if(moveTo == "38") {
-    $("#redSquare").stop(true).animate({"top" : "-=30px"});
-  }
-
-  if(moveTo == "40") {
-    $("#redSquare").stop(true).animate({"top" : "+=30px"});
-  }
-    // slice() method returns portion of array into a new array object
-    keys = keys.slice (1);
-}
-}
-
-moveSquare();
+// screen position
+moveredSquare();
